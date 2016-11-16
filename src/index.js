c6ec4ef3e2baebad95748cc7259ed9c4746e46fb
Boris Gribovskiy <boris.gribovskiy@cyberhull.com>
2016-11-16 18:04:56 +0300

    theory zip file from reactjs theory lesson 4

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-var $ = require('jquery');
-var str = require('./constant');
-require('./less');
-
-var App = function() {
-    // console.log(str);
-    $('body').html(str);
-};
-
-var app = new App();
-app.get = function() {};
-
+console.log('Hi');
